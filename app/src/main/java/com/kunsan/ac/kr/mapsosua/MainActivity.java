a8390f663a4b5b2d2fec47f91607e32e74186af0
DAMMUCK <44718119+DAMMUCK@users.noreply.github.com>
2020-07-03 12:41:06 +0900

    update source

--- app/src/main/java/com/kunsan/ac/kr/mapsosua/MainActivity.java
+++ app/src/main/java/com/kunsan/ac/kr/mapsosua/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.graphics.PointF;
 import android.icu.text.IDNA;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
@@ -33,6 +35,7 @@ import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.overlay.Overlay;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     NaverMap myMap;
     //지오코드 객체 선언
+    String defaultURL = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +56,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         Button choice = (Button) findViewById(R.id.choice);
-        Button btn1 = (Button)findViewById(R.id.basic);
-        Button btn2 = (Button)findViewById(R.id.satellite);
-        Button btn3 = (Button)findViewById(R.id.terrain);
-        LinearLayout layout = (LinearLayout)findViewById(R.id.btnLayout);
+        Button btn1 = (Button) findViewById(R.id.basic);
+        Button btn2 = (Button) findViewById(R.id.satellite);
+        Button btn3 = (Button) findViewById(R.id.terrain);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.btnLayout);
 
         FragmentManager fm = getSupportFragmentManager();
         MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
@@ -69,26 +74,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         choice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(layout.getVisibility()==View.INVISIBLE){
+                if (layout.getVisibility() == View.INVISIBLE) {
                     layout.setVisibility(View.VISIBLE);
-                }else{
+                } else {
                     layout.setVisibility(View.INVISIBLE);
                 }
-                    
+
             }
         });
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (check1==false){
+                if (check1 == false) {
                     myMap.setMapType(NaverMap.MapType.Basic);
-                    Toast.makeText(getApplicationContext(),"기본지도띄우기",Toast.LENGTH_SHORT).show();
-                    check1=true;
-                }else{
+                    Toast.makeText(getApplicationContext(), "기본지도띄우기", Toast.LENGTH_SHORT).show();
+                    check1 = true;
+                } else {
                     myMap.setMapType(NaverMap.MapType.Navi);
-                    Toast.makeText(getApplicationContext(),"네비지도띄우기",Toast.LENGTH_SHORT).show();
-                    check1=false;
+                    Toast.makeText(getApplicationContext(), "네비지도띄우기", Toast.LENGTH_SHORT).show();
+                    check1 = false;
                 }
             }
         });
@@ -96,14 +101,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(check2==false){
+                if (check2 == false) {
                     myMap.setMapType(NaverMap.MapType.Satellite);
-                    Toast.makeText(getApplicationContext(),"위성지도띄우기",Toast.LENGTH_SHORT).show();
-                    check2=true;
-                }else{
+                    Toast.makeText(getApplicationContext(), "위성지도띄우기", Toast.LENGTH_SHORT).show();
+                    check2 = true;
+                } else {
                     myMap.setMapType(NaverMap.MapType.Navi);
-                    Toast.makeText(getApplicationContext(),"네비지도띄우기",Toast.LENGTH_SHORT).show();
-                    check2=false;
+                    Toast.makeText(getApplicationContext(), "네비지도띄우기", Toast.LENGTH_SHORT).show();
+                    check2 = false;
                 }
 
             }
@@ -112,41 +117,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(check3==false){
+                if (check3 == false) {
                     myMap.setMapType(NaverMap.MapType.Terrain);
-                    Toast.makeText(getApplicationContext(),"terrain 지도띄우기",Toast.LENGTH_SHORT).show();
-                    check3=true;
-                }else{
+                    Toast.makeText(getApplicationContext(), "terrain 지도띄우기", Toast.LENGTH_SHORT).show();
+                    check3 = true;
+                } else {
                     myMap.setMapType(NaverMap.MapType.Navi);
-                    Toast.makeText(getApplicationContext(),"네비지도띄우기",Toast.LENGTH_SHORT).show();
-                    check3=false;
+                    Toast.makeText(getApplicationContext(), "네비지도띄우기", Toast.LENGTH_SHORT).show();
+                    check3 = false;
                 }
             }
         });
 
 
-
     }
-
 
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         this.myMap = naverMap;
         myMap.setMapType(NaverMap.MapType.Navi);
-        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new LatLng(35.9452863,126.6799643));
+        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new LatLng(35.9452863, 126.6799643));
         naverMap.moveCamera(cameraUpdate);
-        Toast.makeText(this.getApplicationContext(),"지도띄우기",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this.getApplicationContext(), "지도띄우기", Toast.LENGTH_SHORT).show();
 
         //마커띄우기
         Marker marker = new Marker();
-        marker.setPosition(new LatLng(35.9452863,126.6799643));
+        marker.setPosition(new LatLng(35.9452863, 126.6799643));
         marker.setMap(naverMap);
         Marker marker2 = new Marker();
-        marker2.setPosition(new LatLng(35.9408996,126.6864375));
+        marker2.setPosition(new LatLng(35.9408996, 126.6864375));
         marker2.setMap(naverMap);
         Marker marker3 = new Marker();
-        marker3.setPosition(new LatLng(35.9417,126.6895613));
+        marker3.setPosition(new LatLng(35.9417, 126.6895613));
         marker3.setMap(naverMap);
 
         //정보창
@@ -162,7 +165,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         //마커클릭시 이벤트
         Overlay.OnClickListener listener = overlay -> {
-            Marker mark = (Marker)overlay;
+            Marker mark = (Marker) overlay;
 
             if (mark.getInfoWindow() == null) {
                 // 현재 마커에 정보 창이 열려있지 않을 경우 엶
@@ -175,42 +178,53 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             return true;
         };
 
-        Geocoder geocoder = new Geocoder(this);
 
         marker.setOnClickListener(listener);
         marker2.setOnClickListener(listener);
         marker3.setOnClickListener(listener);
 
+
+        //구글 geocoder
+        /*Geocoder geocoder = new Geocoder(this);
         myMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {//지도 클릭시
             @Override
             public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
                 //지도를 클릭시 클릭한 부분의 위도와 경도를 Toast로 출력
-                String text = "latitude = "+latLng.latitude+" longitude = "+latLng.longitude;
-                Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+                String text = "latitude = " + latLng.latitude + " longitude = " + latLng.longitude;
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
 
 
-
+                //구글 geocoder 사용
                 List<Address> list = null;
-                try{
+                try {
                     list = geocoder.getFromLocation(
                             latLng.latitude,
                             latLng.longitude,
                             10);
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
-                    Log.e("test","입출력 오류 - 서버에서 주소변환시 에러 발생");
+                    Log.e("test", "입출력 오류 - 서버에서 주소변환시 에러 발생");
                 }
-                if(list != null){
-                    if(list.size()==0){
-                        Toast.makeText(getApplicationContext(),"해당되는 주소정보는 없습니다",Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(getApplicationContext(),list.get(0).getPostalCode(),Toast.LENGTH_SHORT).show();
+                if (list != null) {
+                    if (list.size() == 0) {
+                        Toast.makeText(getApplicationContext(), "해당되는 주소정보는 없습니다", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), list.get(0).getPostalCode(), Toast.LENGTH_SHORT).show();
                     }
                 }
             }
+        });*/
+
+        //지도 클릭시 이벤트
+        myMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+
+                //String strCoord = String.valueOf(latLng.longitude) + "," + String.valueOf(latLng.latitude);
+                //AsyncTaskThread asyn = new AsyncTaskThread();
+                //asyn.doInBackground(strCoord);
+                new AsyncTaskThread().execute();
+            }
         });
-
     }
-
-
 }
