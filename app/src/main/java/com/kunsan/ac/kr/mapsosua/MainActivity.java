44f708f8214c67c8bd9239ab60b038d6251633e9
DAMMUCK <44718119+DAMMUCK@users.noreply.github.com>
2020-07-06 12:46:48 +0900

    update MainActivity

--- app/src/main/java/com/kunsan/ac/kr/mapsosua/MainActivity.java
+++ app/src/main/java/com/kunsan/ac/kr/mapsosua/MainActivity.java
@@ -34,7 +34,14 @@ import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.overlay.Overlay;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +55,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     NaverMap myMap;
     //지오코드 객체 선언
     String defaultURL = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc";
-
+    //json 리턴값 저장할 변수
+    private String result="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -152,6 +160,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         marker3.setPosition(new LatLng(35.9417, 126.6895613));
         marker3.setMap(naverMap);
 
+        Marker[] markerArray = new Marker[100];
+        //지도 클릭시 이벤트
+        myMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+                AsyncTaskThread async = new AsyncTaskThread();
+                try{
+                    result = async.execute(latLng).get();
+                    System.out.println(result);
+                    System.out.println(latLng);
+                    markerArray[0].setPosition(new LatLng(latLng.latitude,latLng.longitude));
+                    markerArray[0].setMap(naverMap);
+
+                    /*JSONObject obj = async.execute(latLng).get();
+                    System.out.println(obj);
+                    String postCode = obj.getString("postcode");
+                    System.out.println(postCode);*/
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         //정보창
         InfoWindow infoWindow = new InfoWindow();
         infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
@@ -215,16 +246,49 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });*/
 
-        //지도 클릭시 이벤트
-        myMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
 
-                //String strCoord = String.valueOf(latLng.longitude) + "," + String.valueOf(latLng.latitude);
-                //AsyncTaskThread asyn = new AsyncTaskThread();
-                //asyn.doInBackground(strCoord);
-                new AsyncTaskThread().execute();
-            }
-        });
+
     }
+
+    //asyncTask 스레드 사용해서 json받아오기기
+   private class AsyncTaskThread extends AsyncTask<LatLng,Void, String> {
+
+        @Override
+        protected String doInBackground(LatLng...latLngs) {
+
+            String strCoord = String.valueOf(latLngs[0].longitude) + "," + String.valueOf(latLngs[0].latitude);
+            StringBuilder sb = new StringBuilder();
+
+            StringBuilder urlBuilder = new StringBuilder("https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=" +strCoord+ "&sourcecrs=epsg:4326&output=json&orders=addr");
+
+            try {
+                URL url = new URL(urlBuilder.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Content-type", "application/json");
+                conn.setRequestProperty("X-NCP-APIGW-API-KEY-ID", "d6nvpc15uv");
+                conn.setRequestProperty("X-NCP-APIGW-API-KEY", "fzGdGvib2YvAL8I3KAnS4PGCubwhLNsq8yHIO8o5");
+                InputStream contentStream = conn.getInputStream();
+
+                BufferedReader rd;
+                if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300){
+                    rd = new BufferedReader((new InputStreamReader(conn.getInputStream())));
+                }else{
+                    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                }
+                String line;
+                while((line = rd.readLine()) != null){
+                    sb.append(line);
+                }
+                rd.close();
+                conn.disconnect();
+                return sb.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 }
